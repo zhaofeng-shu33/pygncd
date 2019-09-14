05132e73ad31745a81a0a12555025313656f22b8
zhaofeng-shu33 <616545598@qq.com>
2019-09-14 23:12:00 +0800

    stage dev results

--- pygncd.pyx
+++ pygncd.pyx
@@ -3,7 +3,7 @@ from libc.stdlib cimport malloc, free
 cdef extern from "gn/network.h":
     struct NETWORK
     struct LABELLIST:
-        pass
+         LABELLIST* next
     void construct_network(NETWORK**, double**, int, int)
 
 cdef extern from "gn/gn.h":
@@ -13,12 +13,12 @@ cdef gn_inner_routine(networkx_graph):
     cdef int nedges, nvertices
     cdef int i
     cdef NETWORK* network
-    cdef LABELLIST* label_list
+    cdef LABELLIST* label_header
     cdef double** array_list
     nedges = len(networkx_graph.nodes)
     nvertices = len(networkx_graph.edges)
     array_list = <double**>malloc(sizeof(double*) * nedges)
-    label_list = <LABELLIST*>malloc(sizeof(LABELLIST))
+    label_header = <LABELLIST*>malloc(sizeof(LABELLIST))
     for i in range(nedges):
         array_list[i] = <double*>malloc(sizeof(double*) * 3)
     
@@ -30,8 +30,11 @@ cdef gn_inner_routine(networkx_graph):
         i += 1
     
     construct_network(&network, array_list, nedges, nvertices)
-    girvan_newman(network, label_list)
-    
+    girvan_newman(network, label_header)
+    #while label_header.next != NULL:
+    #    label_header = label_header.next
+    #while label_header->prev != NULL:
+    #    pass
     for i in range(nedges):
         free(array_list[i])
     free(array_list)
